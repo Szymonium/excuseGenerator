e446d1a463dbac67dbdd4a99b4d8cb0b7bdabbee
iii_tie <sdnfsdn@sdnjkfbnsfjkd.com>
2025-04-04 09:38:36 +0200

    Expand form

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Form from "./Form.tsx";
+import Excuses from "./Excuses.tsx";
 
 function App() {
 
   return (
     <>
         <Form />
+        <Excuses />
     </>
   )
 }
--- src/Form.tsx
+++ src/Form.tsx
@@ -1,14 +1,34 @@
 import * as React from "react";
+import {useState} from "react";
 
-function Form() {
+export default function Form() {
+    const [range, setRange] = useState(0);
 
     return (
         <>
             <form onSubmit={(e: React.FormEvent<HTMLFormElement>) => {e.preventDefault()}}>
+                <input type={"text"} name={"name"} placeholder={"Enter name"} required/>
+                <select name={"reason"} required>
+                    <option selected hidden>Choose a reason</option>
+                    <option>homework not done</option>
+                    <option>doctor's appointment</option>
+                    <option>oversleeping</option>
+                    <option>lack of will to live</option>
+                </select>
+                <label>Credibility level</label><input type={"range"} name={"credibility"} min={0} max={10} defaultValue={0} onChange={(e) => {setRange(parseInt(e.target.value))}} required/>
+                <output>{range}</output>
+                <label>Date of event</label><input type={"date"} name={"date"}/>
+                <select name={"creativity"} required>
+                    <option selected hidden>Choose a creativity level</option>
+                    <option>lefty liar</option>
+                    <option>flawed fraud</option>
+                    <option>decent deceiver</option>
+                    <option>master manipulator</option>
+                    <option>trustworthy trickster</option>
+                </select>
+                <textarea placeholder={"Enter a comment"} required></textarea>
                 <button type={"submit"}>Submit</button>
             </form>
         </>
     )
 }
-
-export default Form
